642c92f7beb05ac42d7d889c677c945921dd0887
simgksk <Daum_c0m@naver.com>
2023-04-23 10:29:56 +0900

    초기 커밋.

---
+++ 230423_Baekjoon/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _230423_Baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split();
+            long a = long.Parse(str[0]);
+            long b = long.Parse(str[1]);
+            long c = long.Parse(str[2]);
+
+            Console.WriteLine(a+b+c);
+        }
+    }
+}
